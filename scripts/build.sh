b8ddd6c145b7cf89ef4a81458c854ef752befc3f
krishpavan <krishpavan@hotmail.com>
2022-06-30 00:43:26 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,8 @@ ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-system-x86_64
 
 
 #Install the Packer binary :
-yum -y install wget unzip || apt update && apt -y install wget unzip
-latest=$(curl -L -s https://releases.hashicorp.com/packer | grep 'packer_' | sed 's/^.*<.*\">packer_\(.*\)<\/a>/\1/' | head -1)
-wget https://releases.hashicorp.com/packer/${latest}/packer_${latest}_linux_amd64.zip
-unzip packer*.zip
-chmod +x packer
-mv packer /usr/local/bin/
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install packer
 
 packer build ./centos/centos-7.9-x86_64.json

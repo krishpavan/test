0faaf85b1ca946568bdc3b504988b078b75e8398
krishpavan <krishpavan@hotmail.com>
2022-06-30 00:29:12 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 ## virsh install
-if [ -f /etc/debian_version ]; then
-apt-get update && apt-get -y upgrade
-apt-get -y install qemu-kvm libvirt-dev virtinst virt-viewer libguestfs-tools virt-manager uuid-runtime curl linux-source libosinfo-bin
-virsh net-start default
-virsh net-autostart default
-elif [ -f /etc/redhat-release ]; then
 yum -y install epel-release
 yum -y upgrade
 yum -y group install "Virtualization Host"
 yum -y install virt-manager libvirt virt-install qemu-kvm xauth dejavu-lgc-sans-fonts virt-top libguestfs-tools virt-viewer virt-manager curl
 ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-system-x86_64
-fi
+
 
 #Install the Packer binary :
 yum -y install wget unzip || apt update && apt -y install wget unzip

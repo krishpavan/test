5917fe8737b5149785383d1a93a050eb9388a398
krishpavan <krishpavan@hotmail.com>
2022-06-30 17:28:50 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-system-x86_64
 
 
 #Install the Packer binary :
-yum install -y yum-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-yum -y install packer
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install packer
+packer

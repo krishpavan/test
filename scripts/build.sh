04b0f65e94fe54a3f10c52075d6f761b6725b3c1
krishpavan <krishpavan@hotmail.com>
2022-07-01 08:47:03 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-system-x86_64
 
 #Install the Packer binary :
 yum -y install wget unzip 
-wget -q -O - https://releases.hashicorp.com/packer/1.8.2/packer_1.8.2_linux_amd64.zip
+curl -O https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
 unzip packer*.zip
 chmod +x packer
 mv packer /usr/local/bin/

01d0b84c2030a433e92f2b0d117829136442d3a2
krishpavan <krishpavan@hotmail.com>
2022-07-01 22:31:30 -0400

    Update packer.sh

--- scripts/packer.sh
+++ scripts/packer.sh
@@ -1,4 +1,7 @@
-sudo apt-get install -y qemu-system-x86  qemu-kvm qemu libvirt-bin
+sudo apt-get install -y qemu-system-x86_64
+sudo apt-get install -y qemu-kvm 
+sudo apt-get install -y qemu 
+sudo apt-get install -y libvirt-bin
 wget https://dogws.s3.amazonaws.com/packer_1.8.2_linux_amd64.zip
 unzip packer*.zip
 chmod +x packer
